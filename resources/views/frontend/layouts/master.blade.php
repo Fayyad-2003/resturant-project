da65403fec496af93f4f5bd0ecdd04137208b631
fayad sh <fayadsh33.fs@gmail.com>
2026-09-02 23:36:44 +0300

    refactor(auth): update password reset view and add toastr notification
    Update the password reset flow to use the frontend master layout and
    custom styling instead of the guest layout. Added a toastr success
    notification upon successful password reset and updated asset paths
    to use the `asset()` helper in the master layout.

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -7,20 +7,20 @@
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
     <title>FoodPark | @yield('title') </title>
     <link rel="icon" type="image/png">
-    <link rel="stylesheet" href="frontend/css/all.min.css">
-    <link rel="stylesheet" href="frontend/css/bootstrap.min.css">
-    <link rel="stylesheet" href="frontend/css/spacing.css">
-    <link rel="stylesheet" href="frontend/css/slick.css">
-    <link rel="stylesheet" href="frontend/css/nice-select.css">
-    <link rel="stylesheet" href="frontend/css/venobox.min.css">
-    <link rel="stylesheet" href="frontend/css/toastr.min.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/spacing.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css') }}">
 
-    <link rel="stylesheet" href="frontend/css/jquery.exzoom.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.exzoom.css') }}">
 
-    <link rel="stylesheet" href="frontend/css/style.css">
-    <link rel="stylesheet" href="frontend/css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
 
-    <!-- <link rel="stylesheet" href="frontend/css/rtl.css"> -->
+    <!-- <link rel="stylesheet" href="{{ asset('frontend/css/rtl.css') }}"> -->
 </head>
 
 <body>
