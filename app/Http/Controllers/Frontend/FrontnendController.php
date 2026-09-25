ef1d2ffbc300dceb67b6d004554a47583844729e
fayyad sh <fayadsh33.fs@gmail.com>
2026-09-25 13:15:05 +0300

    feat(frontend): display dynamic sliders on home page
    Integrate the Slider model into the FrontendController to fetch active
    sliders and update the home page slider component to iterate through
    the retrieved slider data.

--- app/Http/Controllers/Frontend/FrontnendController.php
+++ app/Http/Controllers/Frontend/FrontnendController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Slider;
 use Illuminate\Contracts\View\View;
 
 class FrontnendController extends Controller
 {
     public function index(): View
     {
-        return view('frontend.pages.home.index');
+        $sliders = Slider::where('status', 1)->get();
+        return view('frontend.pages.home.index', compact('sliders'));
     }
 }
--- resources/views/frontend/pages/home/components/slider.blade.php
+++ resources/views/frontend/pages/home/components/slider.blade.php
@@ -1,7 +1,40 @@
     <!--BANNER START -->
-    <section class="fp__banner" style="background: url()">
+    <section class="fp__banner" style="background: url({{ asset(@$sliders->first()->image) }});">
         <div class="fp__banner_overlay">
-            <div class="row banner_slider"></div>
+            <div class="row banner_slider">
+                @foreach ($sliders as $slider)
+                    <div class="col-12">
+                        <div class="fp__banner_slider">
+                            <div class=" container">
+                                <div class="row">
+                                    <div class="col-xl-5 col-md-5 col-lg-5">
+                                        <div class="fp__banner_img wow fadeInLeft" data-wow-duration="1s">
+                                            <div class="img">
+                                                <img src="{{ asset($slider->image) }}" alt="food item"
+                                                    class="img-fluid w-100">
+                                                <span> {{ $slider->offer }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-5 col-md-7 col-lg-6">
+                                        <div class="fp__banner_text wow fadeInRight" data-wow-duration="1s">
+                                            <h1>{{ $slider->heading }}</h1>
+                                            <h3>{{ $slider->title }}</h3>
+                                            <p>{{ $slider->details }}</p>
+                                            <ul class="d-flex flex-wrap">
+                                                @if ($slider->url)
+                                                    <li><a class="common_btn" href="{{ $slider->url }}">shop now</a>
+                                                    </li>
+                                                @endif
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
     <!-- BANNER END  -->
